af6c2707cc38f5a346413358fb767561d0546377
Adam <samsung3382@outlook.com>
2022-11-04 16:47:25 -0400

    Nucamp First Commit For Week-2 Class Assignment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+import 'font-awesome/css/font-awesome.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <BrowserRouter>
     <App />
+  </BrowserRouter>
   </React.StrictMode>
 );
 
